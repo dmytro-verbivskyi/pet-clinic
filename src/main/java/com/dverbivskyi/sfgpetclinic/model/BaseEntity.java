4b8490e99a55f63356c758a40989a12351b334c9
Dmytro Verbivskyi <dverbovskiy@gmail.com>
2019-01-14 23:54:48 +0200

    owners and vets controller and index pages

--- src/main/java/com/dverbivskyi/sfgpetclinic/model/BaseEntity.java
+++ src/main/java/com/dverbivskyi/sfgpetclinic/model/BaseEntity.java
@@ -1,5 +1,7 @@
 package com.dverbivskyi.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
@@ -8,6 +10,7 @@ import java.io.Serializable;
 public class BaseEntity implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     public Long getId() {
